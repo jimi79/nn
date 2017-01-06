3b6c2b0eeaf5175edfebb42bbf89e02f834f45b4
Jimi@art <you@example.com>
2017-01-06 13:45:00 +0000

    i try modulo now

--- mine/test_nn.py
+++ mine/test_nn.py
@@ -6,8 +6,8 @@ import os
 import pdb
 import random
 
-count=5000
-maxval=10
+count=50000
+maxval=30
 binsize=8
 
 def convertXtoS(inp, ds_inp):
@@ -34,7 +34,7 @@ def datas(count, maxval, binsize):
 		a = random.randrange(1,maxval)
 		b = random.randrange(1,maxval)
 		c = random.randrange(1,maxval)
-		d = a * b  # the result expected is a+b because i know that works (later will try again somethg more complicated)
+		d = a % b + c  # the result expected is a+b because i know that works (later will try again somethg more complicated)
 # le résultat attendu : d= a+b (d c'est le résultat)
 		X1 = [a, b, c]
 		if i == 0:
